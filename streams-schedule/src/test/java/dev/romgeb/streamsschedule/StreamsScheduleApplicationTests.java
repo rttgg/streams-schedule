d29d05feecc988d5ccd21f8cc44b9ffcc849d010
rttgg <37644205+rttgg@users.noreply.github.com>
2022-02-21 21:57:50 -0800

    My First Commit

---
+++ streams-schedule/src/test/java/dev/romgeb/streamsschedule/StreamsScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.romgeb.streamsschedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamsScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
